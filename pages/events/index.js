b1a1cd48e58c29ab146aa5711af287894c7323e2
aidenjyoon <46219072+aidenjyoon@users.noreply.github.com>
2022-01-16 10:02:25 -0800

    create all events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,14 @@
 import Head from "next/head";
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/EventList";
 
 const AllEventsPage = () => {
+  const events = getAllEvents();
+
   return (
-    <div>
-      <h1>The All Events Page Page</h1>
-    </div>
+    <>
+      <EventList items={events} />
+    </>
   );
 };
 
