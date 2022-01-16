5d0811f6dd9d26345171c12c0e193cf0ec6c1d03
aidenjyoon <46219072+aidenjyoon@users.noreply.github.com>
2022-01-16 12:54:52 -0800

    all events with filter by date option

--- components/ui/Button.js
+++ components/ui/Button.js
@@ -3,10 +3,17 @@ import Link from "next/link";
 import styles from "./Button.module.scss";
 
 const Button = (props) => {
+  if (props.link !== undefined) {
+    return (
+      <Link href={props.link}>
+        <a className={styles.btn}>{props.children}</a>
+      </Link>
+    );
+  }
   return (
-    <Link href={props.link}>
-      <a className={styles.btn}>{props.children}</a>
-    </Link>
+    <button className={styles.btn} onClick={props.onClick}>
+      {props.children}
+    </button>
   );
 };
 
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,14 @@
 import Head from "next/head";
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/EventList";
+import EventsSearch from "../../components/events/EventsSearch";
 
 const AllEventsPage = () => {
   const events = getAllEvents();
 
   return (
     <>
+      <EventsSearch />
       <EventList items={events} />
     </>
   );
