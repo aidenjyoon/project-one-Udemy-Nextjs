d758d8d1c005145912671616eb41fc70cb8871cb
aidenjyoon <46219072+aidenjyoon@users.noreply.github.com>
2022-01-17 11:48:48 -0800

    created search handler for when user wants to search by date
    when date is submitted by user now the user is able to be directed to the date and see which events are happening

--- components/events/EventsSearch.js
+++ components/events/EventsSearch.js
@@ -1,17 +1,26 @@
+import { useRef } from "react";
+
 import Button from "../ui/button";
 import styles from "./EventsSearch.module.scss";
 
 const EventsSearch = (props) => {
-  function randomHandler() {
-    console.log("dd");
+  const yearInputRef = useRef();
+  const monthInputRef = useRef();
+
+  function submitHandler(event) {
+    event.preventDefault();
+    const selectedYear = yearInputRef.current.value;
+    const selectedMonth = monthInputRef.current.value;
+
+    props.onSearch(selectedYear, selectedMonth);
   }
 
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={submitHandler}>
       <div className={styles.controls}>
         <div className={styles.control}>
           <label htmlFor="year">Year</label>
-          <select id="year">
+          <select id="year" ref={yearInputRef}>
             <option value="2021">2021</option>
             <option value="2022">2022</option>
           </select>
@@ -19,7 +28,7 @@ const EventsSearch = (props) => {
 
         <div className={styles.control}>
           <label htmlFor="month">Month</label>
-          <select id="month">
+          <select id="month" ref={monthInputRef}>
             <option value="1">January</option>
             <option value="2">February</option>
             <option value="3">March</option>
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,14 +1,21 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/EventList";
 import EventsSearch from "../../components/events/EventsSearch";
 
 const AllEventsPage = () => {
   const events = getAllEvents();
+  const router = useRouter();
+
+  const findEventsHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  };
 
   return (
     <>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </>
   );
