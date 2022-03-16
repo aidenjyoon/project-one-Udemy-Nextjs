45f0ce01de302e366044fd65038f5d987c2be3c4
aidenjyoon <46219072+aidenjyoon@users.noreply.github.com>
2022-03-15 23:54:19 -0700

    added notification to comment
    Comment section also has notification for the status of upload

--- components/comments/Comments.js
+++ components/comments/Comments.js
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
+import NotificationContext from "../../store/notificationContext";
 import styles from "./Comments.module.scss";
 import CommentsList from "./CommentsList";
 import NewComment from "./NewComment";
@@ -8,7 +9,9 @@ const Comments = (props) => {
 
   const [showComments, setShowComments] = useState(false);
   const [comments, setComments] = useState([]);
+  const notificationCtx = useContext(NotificationContext);
 
+  // Gets comments from database
   useEffect(() => {
     if (showComments) {
       fetch("/api/comments/" + eventId)
@@ -23,16 +26,31 @@ const Comments = (props) => {
     setShowComments((prevStatus) => !prevStatus);
   };
 
+  // add comment to db
   const addCommentHandler = (commentData) => {
-    fetch("/api/comments/" + eventId, {
-      method: "POST",
-      body: JSON.stringify(commentData),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data));
+    try {
+      notificationCtx.showNotification({
+        title: "Pending...",
+        message: "Comment is being uploaded...",
+        status: "pending",
+      });
+
+      fetch("/api/comments/" + eventId, {
+        method: "POST",
+        body: JSON.stringify(commentData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          notificationCtx.showNotification({
+            title: "Success!",
+            message: "Comment has been saved in the database.",
+            status: "success",
+          });
+        });
+    } catch (error) {}
   };
 
   return (
